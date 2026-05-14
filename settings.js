5295c3ab9cc2b65d8cf0df24a6e0b24b3af216ab
Pichet-Sael <pichet.sael@gmail.com>
2026-05-15 03:46:22 +0800

    refactor: replace hardcoded MQTT credentials with environment variables

--- settings.js
+++ settings.js
@@ -3,12 +3,6 @@ module.exports = {
     userDir: __dirname,
     httpStatic: __dirname + '/public',
     functionGlobalContext: {
-        // สามารถดึงค่าไปใช้ใน Function Node ได้
-    },
-    // ตั้งค่ารหัสผ่าน MQTT ผ่าน Environment Variables
-    mqttConnectOptions: {
-        user: process.env.MQTT_USER || "iotproject",
-        password: process.env.MQTT_PASS || "Admin123"
     },
     exportGlobalContextKeys: false,
     editorTheme: {
